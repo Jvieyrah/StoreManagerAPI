8d321be99686752496cc894113529efed917ee9d
Joao <me.contrate.aero@gmail.com>
2022-08-15 19:31:20 -0300

    requisito 04 -  criado validações para inserção de dados.

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,11 +1,12 @@
 const express = require('express');
 // const router = express.Router();
 const productController = require('../controllers/productController');
+const validate = require('../middlewares/validate');
 
 const productRoute = express.Router();
 
 productRoute.get('/', productController.getAll);
 productRoute.get('/:id', productController.findByID);
-productRoute.post('/', productController.create);
+productRoute.post('/', validate.productValidator, productController.create);
 
 module.exports = productRoute;
