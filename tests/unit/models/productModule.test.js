56ff4787146e309d9884a3071b67d88ea90a55fd
Joao <me.contrate.aero@gmail.com>
2022-08-14 23:13:04 -0300

    testes da camada model, segundo exercicio

--- tests/unit/models/productModule.test.js
+++ tests/unit/models/productModule.test.js
@@ -1,66 +1,127 @@
 const { expect } = require('chai');
 const { describe } = require('mocha');
-const Sinon = require('sinon');
+const sinon = require('sinon');
 const connection = require('../../../models/connection');
 
 const productModel = require('../../../models/productModel');
 
 describe('Testa se o model.getAll retorna um array de objetos com id e name', () => {
   describe('Caso OK', () => {
-    // before(()=>{
-    //   const resultExecute = []
-    //   Sinon.stub(connection, 'execute').resolves([resultExecute]);
-    // })
+ 
+    before(() => {
+      const resultExecute = [[{
+        "id": 1,
+        "name": "Martelo de Thor",
+      }],
+      [],
+      ];
+      sinon.stub(connection, 'execute').resolves(resultExecute);
+    })
+
     afterEach(() => {
-      // connection.execute.restore();
+      
       Sinon.restore();
     })
-    it('retorna array', async function () {
-      // AAA - ARRANGE - ACT - ASSERT
-      const resultExecute = [];
-      Sinon.stub(connection, 'execute').resolves([resultExecute]);
-
+    it('retorna array', async () => {
+      // AAA - ARRANGE - ACT - ASSERT  
       const resultado = await productModel.getAll();
 
       expect(resultado).to.be.an('array');
     });
-    it('retorna array vazio', async function () {
-      // AAA - ARRANGE - ACT - ASSERT
-      const resultExecute = [];
-      Sinon.stub(connection, 'execute').resolves([resultExecute]);
-
-      const resultado = await productModel.getAll();
-
-      expect(resultado).to.be.empty;
-    });
-    it('Retorna array cheio', async () => {
-      const resultExecute = [{ id: 1, name: 'teste' }];
-      Sinon.stub(connection, 'execute').resolves([resultExecute]);
-
+    it('retorna array populado', async () => {
+      // AAA - ARRANGE - ACT - ASSERT    
       const resultado = await productModel.getAll();
 
       expect(resultado).to.be.not.empty;
     });
+    it('Retorna array contendo objeto', async () => {
+      // const resultExecute = [{ id: 1, name: 'teste' }];
+      const resultado = await productModel.getAll();
+      expect(resultado[0]).to.be.an('object');
+    });
+    it('Retorna array contendo objeto com id e name', async () => {
+      const resultado = await productModel.getAll();
+      expect(resultado[0]).to.have.property('id');
+      expect(resultado[0]).to.have.property('name');
+    });
+  });
+  describe('Caso retorne um erro ', () => {
+
+    before(() => {
+      const resultExecute = [];
+      sinon.stub(connection, 'execute').resolves(resultExecute);
+    })
+
+    afterEach(() => {
+
+      Sinon.restore();
+    })
+    it('retorna array', async () => {
+      // AAA - ARRANGE - ACT - ASSERT  
+      const resultado = await productModel.getAll();
+
+      expect(resultado).to.be.an('array');
+    });
+    it('retorna array vazio', async () => {
+      // AAA - ARRANGE - ACT - ASSERT    
+      const resultado = await productModel.getAll();
+
+      expect(resultado).to.be.empty;
+    });
   });
 });
-describe('Testa se o model.Create retorna um array de objetos com id e name', async () => {
-  it('Retorna array cheio', async () => {
-    const name = 'teste';
-    const resultExecute = [{ id: 1, name: 'teste' }];
-    Sinon.stub(connection, 'execute').resolves([resultExecute]);
-      
-    const resultado = await productModel.create(name);
-    expect(resultado).to.be.an('array');
-    expect(resultado).to.be.not.empty;
-  });
-  it('Retorna array cheio', async () => {
-    const name = 'teste';
-    const resultExecute = [{ id: 1, name: 'teste' }];
-    Sinon.stub(connection, 'execute').resolves([resultExecute]);
+  
 
-    const resultado = await productModel.create(name);
-    expect(resultado).to.be.an('array');
-    expect(response).to.have.a.property('id');
+describe('Testa se o model.findById retorna um array de objetos com o id indicado no e-mail ', async () => {
+  describe('Caso o id seja encontrado', () => {
+
+    before(() => {
+      const resultExecute = [];
+      sinon.stub(connection, 'execute').resolves(resultExecute);
+    })
+
+    afterEach(() => {
+
+      Sinon.restore();
+    })
+
+    it('Retorna array cheio', async () => {
+      const resultado = await productModel.findById(1);
+      expect(resultado).to.be.an('array');
+    });
+
+    it('Retorna array cheio', async () => {
+      const resultado = await productModel.findById(1);
+      expect(resultado).to.be.not.empty;
+
+    });
+    it('Retorna um id', async () => {
+      const resultado = await productModel.findById(1);
+      expect(resultado).to.have.a.property('id');
+    });
   });
+
+  describe('Caso o id não seja encontrado', () => {
+
+    before(() => {
+      const resultExecute = [];
+      sinon.stub(connection, 'execute').resolves(resultExecute);
+    })
+
+    afterEach(() => {
+
+      Sinon.restore();
+    })
+
+    it('Retorna array cheio', async () => {
+      const resultado = await productModel.findById(99);
+      expect(resultado).to.be.an('array');
+    });
+
+    it('Retorna array cheio', async () => {
+      const resultado = await productModel.findById(1);
+      expect(resultado).to.be.empty;
+
+    });
+   });
 });
-
