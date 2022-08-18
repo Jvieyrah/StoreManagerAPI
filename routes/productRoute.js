a8bc3a1fb25fa804f0dcd3395adc393e99959644
Joao <me.contrate.aero@gmail.com>
2022-08-17 22:57:13 -0300

    req 12 - criado endpoint  para deletar produto

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,5 +1,4 @@
 const express = require('express');
-// const router = express.Router();
 const productController = require('../controllers/productController');
 const validateProduct = require('../middlewares/validateProduct');
 
